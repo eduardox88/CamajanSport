f2d0fc95df234f5a726d43fc09d3342acb26886c
eduardox88 <e-madera@outlook.com>
2016-10-25 20:38:38 -0400

    Proyecto MVC

---
+++ CamajanSport/CamajanSport/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CamajanSport
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
